e5759610016388359c671f4997ae8d90c2869fae
joshua splett <joshua.splett+cloud9@gmail.com>
2017-07-31 17:05:39 +0000

    made changes to website so that logging in is booking the ticket

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
-  has_many :profiles
+  has_one :profile
   
   attr_accessor :stripe_card_token
   # If Pro user passes validations (email, password, etc.),
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   # Store customer.id as the customer token and save the user.
   def save_with_subscription
     if valid?
-      customer = Stripe::Customer.create(description: date, description: email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
